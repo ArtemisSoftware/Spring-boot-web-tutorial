08381895454bd01623ec713f89c36117da59e92b
gustavo <gustavo.isaac@gmail.com>
2024-12-17 14:52:55 +0000

    Logging with Spring Boot

--- src/main/kotlin/com/artemissoftware/springboot/todo_web_app/login/LoginController.kt
+++ src/main/kotlin/com/artemissoftware/springboot/todo_web_app/login/LoginController.kt
@@ -1,5 +1,7 @@
 package com.artemissoftware.springboot.todo_web_app.login
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam
 @Controller
 class LoginController {
 
+    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     @RequestMapping("login")
     fun goToLoginPage(@RequestParam name: String, model: ModelMap): String {
-        model.put("name", name)
-        println("Request param: $name")
+        model["name"] = name
+        logger.debug("Request param: $name")
         return "login"
     }
 }
