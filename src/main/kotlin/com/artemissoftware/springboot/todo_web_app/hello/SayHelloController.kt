fec28ec993e336ce4debf67f610d326fbb26d297
gustavo <gustavo.isaac@gmail.com>
2024-12-17 11:31:49 +0000

    MVC Controller to provide HTML response

--- src/main/kotlin/com/artemissoftware/springboot/todo_web_app/hello/SayHelloController.kt
+++ src/main/kotlin/com/artemissoftware/springboot/todo_web_app/hello/SayHelloController.kt
@@ -10,4 +10,19 @@ class SayHelloController {
     @RequestMapping("say-hello")
     @ResponseBody
     fun sayHello() = "Hello PYT"
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    fun sayHelloHtml(): String{
+        val html = StringBuffer()
+        html.append("<html>")
+        html.append("<head>")
+        html.append("<title>Some title</title>")
+        html.append("</head>")
+        html.append("<body>")
+        html.append("<p>My first paragraph.</p>")
+        html.append("</body>")
+        html.append("</html>")
+        return html.toString()
+    }
 }
