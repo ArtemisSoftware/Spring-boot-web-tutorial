a10c327a7263e4ce5303cdb1563169812bb6c3a5
gustavo <gustavo.isaac@gmail.com>
2024-12-18 21:28:23 +0000

    Setting up a New User for Todo Application

--- src/main/kotlin/com/artemissoftware/springboot/todo_web_app/security/SpringSecurityConfiguration.kt
+++ src/main/kotlin/com/artemissoftware/springboot/todo_web_app/security/SpringSecurityConfiguration.kt
@@ -3,6 +3,7 @@ package com.artemissoftware.springboot.todo_web_app.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
@@ -13,14 +14,19 @@ class SpringSecurityConfiguration {
 
     @Bean
     fun createUserDetailsManager(): InMemoryUserDetailsManager{
-        val userDetails = User.builder()
-            .passwordEncoder { passwordEncoder().encode(it) }
-            .username("test")
-            .password("1234")
+        val userDetails1 = createNewUser("Test", "1234")
+        val userDetails2 = createNewUser("Milo", "12")
+
+        return InMemoryUserDetailsManager(userDetails1, userDetails2)
+    }
+
+    private fun createNewUser(username: String, password: String): UserDetails {
+        return User.builder()
+            .passwordEncoder{ input -> passwordEncoder().encode(input) }
+            .username(username)
+            .password(password)
             .roles("USER", "ADMIN")
             .build()
-
-        return InMemoryUserDetailsManager(userDetails)
     }
 
     @Bean
