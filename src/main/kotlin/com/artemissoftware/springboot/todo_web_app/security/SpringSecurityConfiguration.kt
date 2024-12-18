72580f0ef02807c297990658f7beeeb6b72420ee
gustavo <gustavo.isaac@gmail.com>
2024-12-18 21:41:58 +0000

    Configuring Spring Security to Get H2 console Working

--- src/main/kotlin/com/artemissoftware/springboot/todo_web_app/security/SpringSecurityConfiguration.kt
+++ src/main/kotlin/com/artemissoftware/springboot/todo_web_app/security/SpringSecurityConfiguration.kt
@@ -2,11 +2,15 @@ package com.artemissoftware.springboot.todo_web_app.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
@@ -32,5 +36,25 @@ class SpringSecurityConfiguration {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        //All URLs are protected
+        http.authorizeHttpRequests(
+            Customizer { auth -> auth.anyRequest().authenticated() }
+        )
+
+        //A login form is shown for unauthorized requests
+        http.formLogin(withDefaults())
+
+        //CSRF disable
+        http.csrf { csrf -> csrf.disable() }
+
+        //Frames
+        http.headers { header -> header.frameOptions { frameOptions -> frameOptions.disable() } }
+
+        return http.build()
     }
 }
