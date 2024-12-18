f7b11beb850558fd1a919a25ee258752cc8e8a9e
gustavo <gustavo.isaac@gmail.com>
2024-12-18 16:47:04 +0000

    New Todo - Create a new View

--- src/main/kotlin/com/artemissoftware/springboot/todo_web_app/todo/TodoController.kt
+++ src/main/kotlin/com/artemissoftware/springboot/todo_web_app/todo/TodoController.kt
@@ -3,6 +3,7 @@ package com.artemissoftware.springboot.todo_web_app.todo
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.SessionAttributes
 
 @Controller
@@ -15,5 +16,15 @@ class TodoController(
     fun listAllTodos(model: ModelMap): String{
         model.addAttribute("todos", todoService.findByUsername("Yoga"))
         return "listTodos"
+    }
+
+    @RequestMapping(value = ["add-todo"], method = [RequestMethod.GET])
+    fun showNewTodoPage(): String{
+        return "todo"
+    }
+
+    @RequestMapping(value = ["add-todo"], method = [RequestMethod.POST])
+    fun addNewTodo(): String{
+        return "redirect:list-todos"
     }
 }
