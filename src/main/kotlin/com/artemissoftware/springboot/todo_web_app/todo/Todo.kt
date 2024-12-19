6e295e86ea405976a086994d5e95df88435e031f
gustavo <gustavo.isaac@gmail.com>
2024-12-19 18:27:07 +0000

    Making Todo an Entity and Population Todo Data into H2

--- src/main/kotlin/com/artemissoftware/springboot/todo_web_app/todo/Todo.kt
+++ src/main/kotlin/com/artemissoftware/springboot/todo_web_app/todo/Todo.kt
@@ -1,9 +1,15 @@
 package com.artemissoftware.springboot.todo_web_app.todo
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
 import jakarta.validation.constraints.Size
 import java.time.LocalDate
 
+@Entity
 data class Todo(
+    @Id
+    @GeneratedValue
     var id: Int = 0,
     var username: String = "",
     @field:Size(min = 3, message = "Enter at least 10 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO todo (id, username, description, target_date, is_done)
+VALUES (10001, 'Yoga', 'Get AWS Certified', CURRENT_DATE, false);
+
+INSERT INTO todo (id, username, description, target_date, is_done)
+VALUES (10002,'test', 'Get Azure Certified', CURRENT_DATE, false);
+
+INSERT INTO todo (id, username, description, target_date, is_done)
+VALUES (10003,'in28minutes', 'Get GCP Certified', CURRENT_DATE, false);
+
+INSERT INTO todo (id, username, description, target_date, is_done)
+VALUES (10004,'in28minutes', 'Learn DevOps', CURRENT_DATE, false);
